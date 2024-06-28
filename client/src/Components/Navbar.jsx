e3ab7654d5d7f3e4b31f57e6ae2d37eca0e5e2f5
KatrinCl <117113397+KatrinCl@users.noreply.github.com>
2024-06-28 18:16:06 +0300

    new

--- client/src/Components/Navbar.jsx
+++ client/src/Components/Navbar.jsx
@@ -31,12 +31,12 @@ const Navbar = () => {
             <h1>Essence</h1>
             <p>beauty & spa</p>
           </div>
-        <div className="nav-right">
-          <button className='call-p' onClick={toggleOrderCall}>Заказать звонок</button>
-          <button className='call-cont' onClick={toggleOrderCall}><img src='./call.svg' alt=''/></button>
+          <div className="nav-right">
+            <button className='call-p' onClick={toggleOrderCall}>Заказать звонок</button>
+            <button className='call-cont' onClick={toggleOrderCall}><img src='./call.svg' alt='' /></button>
+          </div>
         </div>
-        </div>
-      <hr />
+        <hr />
       </div>
       <div className="nav-bottom">
         <div className="nav-toggle" onClick={toggleMenu}>
